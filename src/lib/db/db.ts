a2b60088597bed01d2b2d9eec8824aeab7c1378f
Jordan Baron <jordannbaron@gmail.com>
2023-12-02 17:37:34 -0500

    initial commit

---
+++ src/lib/db/db.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+
+const client = new Client({
+	host: '127.0.0.1',
+	port: 5432,
+	user: 'postgres',
+	password: 'password',
+	database: 'db_name'
+});
+
+await client.connect();
+export const db = drizzle(client);
